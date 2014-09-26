281aece06595a92f00c964095e703b240a6a0fa1
CrunchyHotDogs <Kcross119@yahoo.ca>
2014-09-26 13:48:08 -0400

    CSS Changes and Web Site Updates
    I added an about us description and added a little more information on
    the home page.  I changed some CSS so the page would be dynamically
    sized.

---
+++ masterPageBottom.php
@@ -0,0 +1,15 @@
+<!--
+-------------------
+Title: masterPageBottom.php
+Version: 1.0
+Date: 09/18/2014
+-------------------
+-->
+
+<div class="clear"></div>
+			<footer>
+					Full Quiver &copy; 2014 All Chooches Reserved
+			</footer>
+		</div>
+	</body>
+</html>
---
+++ masterPageTop.php
@@ -0,0 +1,32 @@
+<!--
+-------------------
+Title: masterPageTop.php
+Version: 1.0
+Date: 09/18/2014
+-------------------
+-->
+
+<html>
+	<head>
+		<title>Full Quiver About Us</title>
+		<link rel="icon" type="image/jpg" href="Images/QuiverIcon.jpg">
+		
+		<link rel="stylesheet" href="indexCSS.css" />
+	
+		<script>
+			
+		</script>
+	</head>
+	<body>
+		<div id="content">
+				<img id="slideshow" src="http://placehold.it/850x200"/>
+				<img id="slideshow" src="http://placehold.it/850x100"/>
+			
+			<div class="mainNav">
+				<a href="index.html">Home</a>
+				<a href="product.html">Products</a>
+				<a href="tutorial.html">Tutorials</a>
+				<span>About Us</span>
+				<a href="contactUs.html">Contact Us</a>
+				<a href="account.html">Account</a>
+			</div>
